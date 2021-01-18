f08c6de9803aef5ab33563beccfa1be027370bdb
Elliott Troop <elliottp731@gmail.com>
2021-01-17 20:04:00 -0500

    Setup date button

--- app/src/main/java/com/troopgang/criminalintent/ui/CrimeFragment.kt
+++ app/src/main/java/com/troopgang/criminalintent/ui/CrimeFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import com.troopgang.criminalintent.R
 import com.troopgang.criminalintent.model.Crime
@@ -15,6 +16,7 @@ class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +28,13 @@ class CrimeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
-        titleField = view.findViewById(R.id.crime_title)
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as Button
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
         return view
     }
 
