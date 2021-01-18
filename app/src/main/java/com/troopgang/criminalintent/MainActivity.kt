2b3641d2a7250e6b5306b3de1253a7e74eb1b817
Elliott Troop <elliottp731@gmail.com>
2021-01-17 20:12:27 -0500

    Added CrimeFragment to MainActivity.kt

--- app/src/main/java/com/troopgang/criminalintent/MainActivity.kt
+++ app/src/main/java/com/troopgang/criminalintent/MainActivity.kt
@@ -2,10 +2,22 @@ package com.troopgang.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.troopgang.criminalintent.ui.CrimeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
