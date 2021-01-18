9c254a2980b5d537aa6d8422479e926bf1b9ac2b
Elliott Troop <elliottp731@gmail.com>
2021-01-17 20:01:39 -0500

    Added textchangedlistener to titleField

--- app/src/main/java/com/troopgang/criminalintent/ui/CrimeFragment.kt
+++ app/src/main/java/com/troopgang/criminalintent/ui/CrimeFragment.kt
@@ -1,23 +1,52 @@
 package com.troopgang.criminalintent.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.troopgang.criminalintent.R
+import com.troopgang.criminalintent.model.Crime
 
 class CrimeFragment : Fragment() {
 
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        crime = Crime()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crime, container, false)
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.crime_title)
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                crime.title = p0.toString()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
     }
 }
